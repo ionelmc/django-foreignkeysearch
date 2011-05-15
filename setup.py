5d8cbbae16a83c30cb3d23e2df4c864cff6a06f3
Ionel Maries Cristian <ionel.mc@gmail.com>
2011-05-16 01:02:26 +0300

    Move files into package and add setup script.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+import os
+
+setup(
+    name = "django-foreignkeysearch",
+    version = "0.1.0",
+    url = '',
+    download_url = '',
+    license = 'BSD',
+    description = "",
+    author = '',
+    author_email = '',
+    packages = find_packages(),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
